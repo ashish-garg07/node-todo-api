361f1df022fb3b977d99672424802d95006bff15
ashish-garg007 <ashish.garg@appirio.com>
2018-04-24 17:46:17 +0530

    added remove mongo db row code

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Ashish on 4/24/18.
+ */
+
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if(err){
+    return console.log('unable to connecte server');
+  }
+  console.log('connected to server');
+const db = client.db('TodoApp');
+
+  // db.collection('Todos').insertOne({
+  //   text:'something to do',
+  //   completed:false
+  // },(err, result)=>{
+  //   if(err){
+  //     return console.log('failed to insert record : ',err);
+  //   }
+  //   console.log('result is : ',JSON.stringify(result.ops,undefined,2));
+  //
+  // });
+
+  db.collection('Todos').findOneAndDelete({text: 'duplicate Data'}).then((result) => {
+    console.log(result);
+  })
+
+  client.close();
+});
